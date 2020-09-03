6ce60a600d9ae0231c7fc14d0f21bf69deb22fef
Steve Hornyak <hornyak@usc.edu>
2020-09-03 13:44:47 -0700

    Recreated file

---
+++ ee250/lab02/grovepi_sensors.py
@@ -0,0 +1,42 @@
+""" EE 250L Lab 02: GrovePi Sensors
+List team members here.
+Steve Hornyak
+Insert Github repository link here.
+"""
+
+"""python3 interpreters in Ubuntu (and other linux distros) will look in a 
+default set of directories for modules when a program tries to `import` one. 
+Examples of some default directories are (but not limited to):
+  /usr/lib/python3.5
+  /usr/local/lib/python3.5/dist-packages
+The `sys` module, however, is a builtin that is written in and compiled in C for
+performance. Because of this, you will not find this in the default directories.
+"""
+import sys
+import time
+# By appending the folder of all the GrovePi libraries to the system path here,
+# we are successfully `import grovepi`
+sys.path.append('../../Software/Python/')
+# This append is to support importing the LCD library.
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+
+import grovepi
+from grove_rgb_lcd import *
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
+be true"""
+if __name__ == '__main__':
+    PORT = 6    # D6
+
+    while True:
+        #So we do not poll the sensors too quickly which may introduce noise,
+        #sleep for a reasonable time of 200ms between each iteration.
+        time.sleep(0.2)
+
+        dist = grovepi.ultrasonicRead(PORT)
+
+        #Print the distance to the rgb lcd
+        setRGB(0, 128, 64)
+        setRGB(0, 255, 0)
+        setText("34cm")
