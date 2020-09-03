287cfe479f0d980a81f86a18b24a398ef75a3a13
Steve Hornyak <hornyak@usc.edu>
2020-09-03 12:50:23 -0700

    Adding in the RGB

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -23,6 +23,7 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
@@ -35,4 +36,9 @@ if __name__ == '__main__':
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        dist = grovepi.ultrasonicRead(PORT)
+
+        #Print the distance to the rgb lcd
+        setRGB(0, 128, 64)
+        setRGB(0, 255, 0)
+        setText(dist + "cm")
